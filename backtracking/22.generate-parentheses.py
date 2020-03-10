8f138abee2d4f08685d8cf4a567b76143b112204
exshak <sbijabhai@gmail.com>
2020-03-09 23:53:29 -0400

    generate parentheses

---
+++ backtracking/22.generate-parentheses.py
@@ -0,0 +1,39 @@
+#
+# @lc app=leetcode id=22 lang=python3
+#
+# [22] Generate Parentheses
+#
+# https://leetcode.com/problems/generate-parentheses/description/
+#
+# algorithms
+# Medium (59.54%)
+# Likes:    4215
+# Dislikes: 233
+# Total Accepted:    477.4K
+# Total Submissions: 795.9K
+# Testcase Example:  '3'
+#
+# 
+# Given n pairs of parentheses, write a function to generate all combinations
+# of well-formed parentheses.
+# 
+# 
+# 
+# For example, given n = 3, a solution set is:
+# 
+# 
+# [
+# ⁠ "((()))",
+# ⁠ "(()())",
+# ⁠ "(())()",
+# ⁠ "()(())",
+# ⁠ "()()()"
+# ]
+# 
+#
+
+# @lc code=start
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        
+# @lc code=end

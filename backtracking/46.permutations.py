644ff2b0799ea85a07832c0f4692e380fdeb9541
exshak <sbijabhai@gmail.com>
2020-03-11 23:38:11 -0400

    permutations

---
+++ backtracking/46.permutations.py
@@ -0,0 +1,39 @@
+#
+# @lc app=leetcode id=46 lang=python3
+#
+# [46] Permutations
+#
+# https://leetcode.com/problems/permutations/description/
+#
+# algorithms
+# Medium (60.01%)
+# Likes:    3151
+# Dislikes: 94
+# Total Accepted:    525.2K
+# Total Submissions: 867.5K
+# Testcase Example:  '[1,2,3]'
+#
+# Given a collection of distinct integers, return all possible permutations.
+# 
+# Example:
+# 
+# 
+# Input: [1,2,3]
+# Output:
+# [
+# ⁠ [1,2,3],
+# ⁠ [1,3,2],
+# ⁠ [2,1,3],
+# ⁠ [2,3,1],
+# ⁠ [3,1,2],
+# ⁠ [3,2,1]
+# ]
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+# @lc code=end

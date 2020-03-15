e50ee856791ba4ace8b48f7bcb6840da617835a6
exshak <sbijabhai@gmail.com>
2020-03-14 23:10:08 -0400

    merge sorted array

---
+++ two-pointers/88.merge-sorted-array.py
@@ -0,0 +1,46 @@
+#
+# @lc app=leetcode id=88 lang=python3
+#
+# [88] Merge Sorted Array
+#
+# https://leetcode.com/problems/merge-sorted-array/description/
+#
+# algorithms
+# Easy (38.00%)
+# Likes:    1730
+# Dislikes: 3633
+# Total Accepted:    502.7K
+# Total Submissions: 1.3M
+# Testcase Example:  '[1,2,3,0,0,0]\n3\n[2,5,6]\n3'
+#
+# Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as
+# one sorted array.
+# 
+# Note:
+# 
+# 
+# The number of elements initialized in nums1 and nums2 are m and n
+# respectively.
+# You may assume that nums1 has enough space (size that is greater or equal to
+# m + n) to hold additional elements from nums2.
+# 
+# 
+# Example:
+# 
+# 
+# Input:
+# nums1 = [1,2,3,0,0,0], m = 3
+# nums2 = [2,5,6],       n = 3
+# 
+# Output: [1,2,2,3,5,6]
+# 
+#
+
+# @lc code=start
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        
+# @lc code=end

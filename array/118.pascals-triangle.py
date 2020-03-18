e67e8171c7499d1edd7e9347ea0639c74c68555c
exshak <sbijabhai@gmail.com>
2020-03-17 23:45:15 -0400

    pascal's triangle

---
+++ array/118.pascals-triangle.py
@@ -0,0 +1,43 @@
+#
+# @lc app=leetcode id=118 lang=python3
+#
+# [118] Pascal's Triangle
+#
+# https://leetcode.com/problems/pascals-triangle/description/
+#
+# algorithms
+# Easy (49.92%)
+# Likes:    1112
+# Dislikes: 91
+# Total Accepted:    339.7K
+# Total Submissions: 674.8K
+# Testcase Example:  '5'
+#
+# Given a non-negative integer numRows, generate the first numRows of Pascal's
+# triangle.
+# 
+# 
+# In Pascal's triangle, each number is the sum of the two numbers directly
+# above it.
+# 
+# Example:
+# 
+# 
+# Input: 5
+# Output:
+# [
+# ⁠    [1],
+# ⁠   [1,1],
+# ⁠  [1,2,1],
+# ⁠ [1,3,3,1],
+# ⁠[1,4,6,4,1]
+# ]
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        
+# @lc code=end

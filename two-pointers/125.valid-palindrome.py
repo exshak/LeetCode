d5f4d4928fea520453fd1c5aeabd7e3ba37aa6f5
exshak <sbijabhai@gmail.com>
2020-03-18 23:41:36 -0400

    valid palindrome

---
+++ two-pointers/125.valid-palindrome.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode id=125 lang=python3
+#
+# [125] Valid Palindrome
+#
+# https://leetcode.com/problems/valid-palindrome/description/
+#
+# algorithms
+# Easy (33.96%)
+# Likes:    942
+# Dislikes: 2576
+# Total Accepted:    504.3K
+# Total Submissions: 1.5M
+# Testcase Example:  '"A man, a plan, a canal: Panama"'
+#
+# Given a string, determine if it is a palindrome, considering only
+# alphanumeric characters and ignoring cases.
+# 
+# Note: For the purpose of this problem, we define empty string as valid
+# palindrome.
+# 
+# Example 1:
+# 
+# 
+# Input: "A man, a plan, a canal: Panama"
+# Output: true
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: "race a car"
+# Output: false
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+# @lc code=end

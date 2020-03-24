f88c9f69d97fd121f64f74701255ab7f62b12f91
exshak <sbijabhai@gmail.com>
2020-03-23 23:40:11 -0400

    count primes

---
+++ hash-table/204.count-primes.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=204 lang=python3
+#
+# [204] Count Primes
+#
+# https://leetcode.com/problems/count-primes/description/
+#
+# algorithms
+# Easy (30.53%)
+# Likes:    1643
+# Dislikes: 528
+# Total Accepted:    318.5K
+# Total Submissions: 1M
+# Testcase Example:  '10'
+#
+# Count the number of prime numbers less than a non-negative number, n.
+# 
+# Example:
+# 
+# 
+# Input: 10
+# Output: 4
+# Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        
+# @lc code=end

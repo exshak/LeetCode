cde3dc8ff11e6b4156c58717e2ea51a54424d221
exshak <sbijabhai@gmail.com>
2020-03-25 23:31:05 -0400

    palindrome linked list

---
+++ linked-list/234.palindrome-linked-list.py
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode id=234 lang=python3
+#
+# [234] Palindrome Linked List
+#
+# https://leetcode.com/problems/palindrome-linked-list/description/
+#
+# algorithms
+# Easy (37.89%)
+# Likes:    2544
+# Dislikes: 329
+# Total Accepted:    365.5K
+# Total Submissions: 958.2K
+# Testcase Example:  '[1,2]'
+#
+# Given a singly linked list, determine if it is a palindrome.
+# 
+# Example 1:
+# 
+# 
+# Input: 1->2
+# Output: false
+# 
+# Example 2:
+# 
+# 
+# Input: 1->2->2->1
+# Output: true
+# 
+# Follow up:
+# Could you do it in O(n) time and O(1) space?
+# 
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        
+# @lc code=end

138f70f0622bdf5666e82035de2cafca41e9f9cc
exshak <sbijabhai@gmail.com>
2020-03-28 23:12:20 -0400

    reverse string

---
+++ two-pointers/344.reverse-string.py
@@ -0,0 +1,53 @@
+#
+# @lc app=leetcode id=344 lang=python3
+#
+# [344] Reverse String
+#
+# https://leetcode.com/problems/reverse-string/description/
+#
+# algorithms
+# Easy (65.53%)
+# Likes:    1155
+# Dislikes: 643
+# Total Accepted:    624.9K
+# Total Submissions: 947.4K
+# Testcase Example:  '["h","e","l","l","o"]'
+#
+# Write a function that reverses a string. The input string is given as an
+# array of characters char[].
+# 
+# Do not allocate extra space for another array, you must do this by modifying
+# the input array in-place with O(1) extra memory.
+# 
+# You may assume all the characters consist of printable ascii characters.
+# 
+# 
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: ["h","e","l","l","o"]
+# Output: ["o","l","l","e","h"]
+# 
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: ["H","a","n","n","a","h"]
+# Output: ["h","a","n","n","a","H"]
+# 
+# 
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        
+# @lc code=end

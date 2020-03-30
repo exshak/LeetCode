9b50c0b3995de031d99692fb993bf70a7a229ad6
exshak <sbijabhai@gmail.com>
2020-03-29 23:22:50 -0400

    sum of two integers

---
+++ bit-manipulation/371.sum-of-two-integers.py
@@ -0,0 +1,43 @@
+#
+# @lc app=leetcode id=371 lang=python3
+#
+# [371] Sum of Two Integers
+#
+# https://leetcode.com/problems/sum-of-two-integers/description/
+#
+# algorithms
+# Easy (50.68%)
+# Likes:    1060
+# Dislikes: 1963
+# Total Accepted:    171K
+# Total Submissions: 337.6K
+# Testcase Example:  '1\n2'
+#
+# Calculate the sum of two integers a and b, but you are not allowed to use the
+# operator + and -.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: a = 1, b = 2
+# Output: 3
+# 
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: a = -2, b = 3
+# Output: 1
+# 
+# 
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        
+# @lc code=end

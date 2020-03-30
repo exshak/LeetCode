1395f18d8fd02c840ec615bd82dc74aac5fee8dd
exshak <sbijabhai@gmail.com>
2020-03-29 23:21:58 -0400

    intersection of two arrays ii

---
+++ hash-table/350.intersection-of-two-arrays-ii.py
@@ -0,0 +1,58 @@
+#
+# @lc app=leetcode id=350 lang=python3
+#
+# [350] Intersection of Two Arrays II
+#
+# https://leetcode.com/problems/intersection-of-two-arrays-ii/description/
+#
+# algorithms
+# Easy (50.23%)
+# Likes:    1104
+# Dislikes: 360
+# Total Accepted:    302.8K
+# Total Submissions: 599.8K
+# Testcase Example:  '[1,2,2,1]\n[2,2]'
+#
+# Given two arrays, write a function to compute their intersection.
+# 
+# Example 1:
+# 
+# 
+# Input: nums1 = [1,2,2,1], nums2 = [2,2]
+# Output: [2,2]
+# 
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+# Output: [4,9]
+# 
+# 
+# Note:
+# 
+# 
+# Each element in the result should appear as many times as it shows in both
+# arrays.
+# The result can be in any order.
+# 
+# 
+# Follow up:
+# 
+# 
+# What if the given array is already sorted? How would you optimize your
+# algorithm?
+# What if nums1's size is small compared to nums2's size? Which algorithm is
+# better?
+# What if elements of nums2 are stored on disk, and the memory is limited such
+# that you cannot load all elements into the memory at once?
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+# @lc code=end
